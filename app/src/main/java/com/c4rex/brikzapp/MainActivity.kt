f48527c56351d28135976418ebbb7efb85812330
tatisgordon <tatisgordon@gmail.com>
2020-10-10 20:01:02 -0500

    comm

--- app/src/main/java/com/c4rex/brikzapp/MainActivity.kt
+++ app/src/main/java/com/c4rex/brikzapp/MainActivity.kt
@@ -40,7 +40,6 @@ class MainActivity : AppCompatActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Greeting("Brikz")
-                        WinButton()
                         RenderChallengeBtn(player)
                         Spacer(Modifier.preferredSize(26.dp))
 
