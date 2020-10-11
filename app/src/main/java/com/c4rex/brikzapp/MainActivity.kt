db55499181fe4ef7221aa59faa74905c35abed15
boris <boris@c4rex.dev>
2020-10-11 03:16:23 +0200

    updated

--- app/src/main/java/com/c4rex/brikzapp/MainActivity.kt
+++ app/src/main/java/com/c4rex/brikzapp/MainActivity.kt
@@ -3,23 +3,22 @@ package com.c4rex.brikzapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.contentColor
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.ui.tooling.preview.Preview
-import com.c4rex.brikzapp.lead.activity.LeadActivity
-import com.c4rex.brikzapp.lead.activity.WinActivity
-import com.c4rex.brikzapp.recognition.activities.RecognitionActivity
 import com.c4rex.brikzapp.level.SelectLevelActivity
 import com.c4rex.brikzapp.player.PlayerMockSource
 import com.c4rex.brikzapp.player.PlayerModel
@@ -32,19 +31,22 @@ class MainActivity : AppCompatActivity() {
         val player = PlayerMockSource.getArtistMock()
         setContent {
             BrikzAppTheme {
+                Image(
+                    asset = imageResource(id = R.drawable.background),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = Color.Transparent) {
                     Column(
                         modifier = Modifier.fillMaxWidth().fillMaxHeight(),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Greeting("Brikz")
-
                         RenderChallengeBtn(player)
                         Spacer(Modifier.preferredSize(26.dp))
-
-                        RecognitionImmerseButton()
+                        TimeAttackButton()
                     }
                 }
             }
@@ -54,72 +56,24 @@ class MainActivity : AppCompatActivity() {
     @Composable
     private fun RenderChallengeBtn(player:PlayerModel) {
         Button(
+            contentColor = Color.White,
             onClick = { startActivity(
                 Intent(this@MainActivity, SelectLevelActivity::class.java).apply
                 { putExtra("player", player) }
-            )}
+            )
+            }
         ) {
             Text(text = "Challenge", style = typography.h2)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Composable
-fun WinButton() {
-    val context = ContextAmbient.current
-
-    Button(onClick = {
-        startActivity(
-                context,
-                Intent(
-                        context, WinActivity::class.java
-                ),
-                null
-        )
-    },backgroundColor = Color.Red) {
-        Text("Win Activity Test")
-    }
-
-}
-@Composable
-fun LeadButton() {
-    val context = ContextAmbient.current
-
-    Button(onClick = {
-        startActivity(
-            context,
-            Intent(
-                context, LeadActivity::class.java
-            ),
-            null
-        )
-    },backgroundColor = Color.Red) {
-        Text("laed Activity Test")
-    }
-
-}
 @Preview(showBackground = true)
 @Composable
-fun RecognitionImmerseButton() {
-    val context = ContextAmbient.current
-
-    Button(onClick = {
-            startActivity(
-                context,
-                Intent(
-                    context, RecognitionActivity::class.java
-                ),
-                null
-        )
-    },backgroundColor = Color.Red) {
-        Text("Wikitude", style = typography.h2)
+fun TimeAttackButton() {
+    Button(backgroundColor = Color.Magenta, onClick = {}, contentColor = Color.White) {
+        Text("Time Attack", style = typography.h2)
     }
 
 }
-
-
